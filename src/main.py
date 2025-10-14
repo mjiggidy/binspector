338346c4a4740c7dcb5753bfa4856d6354d3970e
Michael Jordan <michael@glowingpixel.com>
2025-10-14 11:59:30 -0700

    Nuitka cleanup

--- src/main.py
+++ src/main.py
@@ -5,13 +5,12 @@ Binspector Bootstrap (bb4u)
 # Compilation configuration for Nuitka
 # ---
 # nuitka-project: --mode=app
-# ;nuitka-project: --onefile-tempdir-spec="{CACHE_DIR}/{COMPANY}/{PRODUCT}/{VERSION}"
+# nuitka-project: --onefile-tempdir-spec="{CACHE_DIR}/{COMPANY}/{PRODUCT}/{VERSION}"
 # nuitka-project: --deployment
 # nuitka-project: --output-filename="binspector"
 # nuitka-project: --output-dir="dist"
 # nuitka-project: --remove-output
 # nuitka-project: --plugin-enable=pyside6
-### nuitka-project: --include-qt-plugins=sqldrivers
 # nuitka-project: --noinclude-setuptools-mode="nofollow"
 # ---
 
