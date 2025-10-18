b57c0c5b6b40a8cb59780611353700cfbdbaf19d
Michael Jordan <michael@glowingpixel.com>
2025-10-18 11:34:36 -0700

    Just little things

--- src/binspector/views/binframeview.py
+++ src/binspector/views/binframeview.py
@@ -2,3 +2,11 @@ from PySide6 import QtCore, QtGui, QtWidgets
 
 class BSBinFrameView(QtWidgets.QGraphicsView):
 	"""Frame view for an Avid bin"""
+
+	def __init__(self, *args, **kwargs):
+
+		super().__init__(*args, **kwargs)
+
+		self.setScene(QtWidgets.QGraphicsScene())
+
+		self.scene().addText("Look At This Frame View Haha Wow")
