f31cad3a2cfa3d77a73aec8a88f476a80b2f8f44
Michael Jordan <michael@glowingpixel.com>
2025-10-22 12:02:03 -0700

    Formatting

--- src/binspector/views/treeview.py
+++ src/binspector/views/treeview.py
@@ -6,7 +6,6 @@ from ..models import viewmodels
 class LBTreeView(QtWidgets.QTreeView):
 	"""QTreeView but nicer"""
 
-
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
@@ -33,27 +32,6 @@ class LBTreeView(QtWidgets.QTreeView):
 	def resizeAllColumnsToContents(self):
 		for idx in range(self.header().count()):
 			self.resizeColumnToContents(idx)
-
-	@QtCore.Slot(object)
-	def setColumnWidths(self, column_widths:dict[str,int]):
-
-		raise DeprecationWarning("Nah, use bintreeview")
-
-		if not column_widths:
-
-			self.resizeAllColumnsToContents()
-			return
-		
-		column_names = self.columnDisplayNames()
-
-		for col, width in column_widths.items():
-		
-			try:
-				col_idx = column_names.index(col)
-			except ValueError:
-				continue
-		
-			self.setColumnWidth(col_idx, width)
 	
 	@QtCore.Slot(str, QtCore.Qt.SortOrder)
 	def sortByColumnName(self, column_name:str, sort_order:QtCore.Qt.SortOrder) -> bool:
