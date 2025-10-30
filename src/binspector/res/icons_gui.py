7a11f52cc524863d94fa44a792c798f319dbdff3
Michael Jordan <michael@glowingpixel.com>
2025-10-30 12:31:49 -0700

    Themed icon begin

--- src/binspector/res/icons_gui.py
+++ src/binspector/res/icons_gui.py
@@ -58,7 +58,7 @@ H 10 Z M 1,4 H 5\
 1 H 6 Z m 0,2 h \
 9 v 1 H 6 Z\x22 />\x0a\
   </g>\x0a</svg>\
-\x00\x00\x04J\
+\x00\x00\x06\x02\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
@@ -79,56 +79,84 @@ org/2000/svg\x22\x0a  \
 ://www.w3.org/20\
 00/svg\x22>\x0a  <defs\
 \x0a     id=\x22defs1\x22\
- />\x0a  <g\x0a     in\
-kscape:label=\x22La\
-yer 1\x22\x0a     inks\
-cape:groupmode=\x22\
-layer\x22\x0a     id=\x22\
-layer1\x22>\x0a    <pa\
-th\x0a       id=\x22pa\
-th1\x22\x0a       stro\
-ke=\x22none\x22\x0a      \
- fill=\x22{ButtonTe\
-xt}\x22\x0a       d=\x22M\
- 11.5,3 C 11.223\
-,3 11,3.446 11,4\
- v 11 c 0,0.554 \
-0.223,1 0.5,1 0.\
-277,0 0.5,-0.446\
- 0.5,-1 V 4 C 12\
-,3.446 11.777,3 \
-11.5,3 Z M 2,0 C\
- 0.892,0 0,0.892\
- 0,2 v 12 c 0,1.\
-108 0.892,2 2,2 \
-h 9.5 C 11.223,1\
-6 11,15.554 11,1\
-5 H 5 v -2 h 6 V\
- 12 H 5 v -2 h 6\
- V 9 H 5 V 7 h 6\
- V 6 H 5 V 4 h 6\
- C 11,3.446 11.2\
-23,3 11.5,3 H 11\
- V 1 h 1 V 3 H 1\
-1.5 C 11.777,3 1\
-2,3.446 12,4 h 3\
- v 2 h -3 v 1 h \
-3 v 2 h -3 v 1 h\
- 3 v 2 h -3 v 1 \
-h 3 v 2 h -3 c 0\
-,0.554 -0.223,1 \
--0.5,1 H 14 c 1.\
-108,0 2,-0.892 2\
-,-2 V 2 C 16,0.8\
-92 15.108,0 14,0\
- Z M 4,1 H 5 V 3\
- H 4 Z M 1,4 H 4\
- V 6 H 1 Z M 1,7\
- H 4 V 9 H 1 Z m\
- 0,3 h 3 v 2 H 1\
- Z m 0,3 h 3 v 2\
- H 1 Z\x22 />\x0a  </g\
->\x0a</svg>\x0a\
+ />\x0a  <g\x0a     id\
+=\x22layer1\x22>\x0a    <\
+rect\x0a       styl\
+e=\x22fill:{Base}\x22\x0a\
+       id=\x22rect1\
+\x22\x0a       width=\x22\
+14\x22\x0a       heigh\
+t=\x222\x22\x0a       x=\x22\
+1\x22\x0a       y=\x224\x22 \
+/>\x0a    <rect\x0a   \
+    style=\x22fill:\
+{Base}\x22\x0a       i\
+d=\x22rect2\x22\x0a      \
+ width=\x2214\x22\x0a    \
+   height=\x222\x22\x0a  \
+     x=\x221\x22\x0a     \
+  y=\x2210\x22 />\x0a    \
+<rect\x0a       sty\
+le=\x22fill:{Altern\
+ateBase}\x22\x0a      \
+ id=\x22rect1-2\x22\x0a  \
+     width=\x2214\x22\x0a\
+       height=\x222\
+\x22\x0a       x=\x221\x22\x0a \
+      y=\x227\x22 />\x0a \
+   <rect\x0a       \
+style=\x22fill:{Alt\
+ernateBase}\x22\x0a   \
+    id=\x22rect3\x22\x0a \
+      width=\x2214\x22\
+\x0a       height=\x22\
+2\x22\x0a       x=\x221\x22\x0a\
+       y=\x2213\x22 />\
+\x0a    <path\x0a     \
+  id=\x22path1\x22\x0a   \
+    stroke=\x22none\
+\x22\x0a       fill=\x22{\
+ButtonText}\x22\x0a   \
+    d=\x22M 11.5,3 \
+C 11.223,3 11,3.\
+446 11,4 v 11 c \
+0,0.554 0.223,1 \
+0.5,1 0.277,0 0.\
+5,-0.446 0.5,-1 \
+V 4 C 12,3.446 1\
+1.777,3 11.5,3 Z\
+ M 2,0 C 0.892,0\
+ 0,0.892 0,2 v 1\
+2 c 0,1.108 0.89\
+2,2 2,2 h 9.5 C \
+11.223,16 11,15.\
+554 11,15 H 5 v \
+-2 h 6 V 12 H 5 \
+v -2 h 6 V 9 H 5\
+ V 7 h 6 V 6 H 5\
+ V 4 h 6 C 11,3.\
+446 11.223,3 11.\
+5,3 H 11 V 1 h 1\
+ V 3 H 11.5 C 11\
+.777,3 12,3.446 \
+12,4 h 3 v 2 h -\
+3 v 1 h 3 v 2 h \
+-3 v 1 h 3 v 2 h\
+ -3 v 1 h 3 v 2 \
+h -3 c 0,0.554 -\
+0.223,1 -0.5,1 H\
+ 14 c 1.108,0 2,\
+-0.892 2,-2 V 2 \
+C 16,0.892 15.10\
+8,0 14,0 Z M 4,1\
+ H 5 V 3 H 4 Z M\
+ 1,4 H 4 V 6 H 1\
+ Z M 1,7 H 4 V 9\
+ H 1 Z m 0,3 h 3\
+ v 2 H 1 Z m 0,3\
+ h 3 v 2 H 1 Z\x22 \
+/>\x0a  </g>\x0a</svg>\
+\x0a\
 \x00\x00\x02\x81\
 <\
 ?xml version=\x221.\
@@ -205,10 +233,10 @@ qt_resource_struct = b"\
 \x00\x00\x00\x10\x00\x02\x00\x00\x00\x03\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00@\x00\x00\x00\x00\x00\x01\x00\x00\x03\x22\
-\x00\x00\x01\x9a6m5\xe7\
+\x00\x00\x01\x9a6\x8d,\xab\
 \x00\x00\x00\x1c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 \x00\x00\x01\x9a6n/\xe5\
-\x00\x00\x00`\x00\x00\x00\x00\x00\x01\x00\x00\x07p\
+\x00\x00\x00`\x00\x00\x00\x00\x00\x01\x00\x00\x09(\
 \x00\x00\x01\x9a6n\xff\xbe\
 "
 
