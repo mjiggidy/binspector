46d24f6c994f519ced8f148e2c957075ed009d37
Michael Jordan <michael@glowingpixel.com>
2025-10-30 12:06:38 -0700

    A bit more concise

--- src/binspector/res/icons_gui.py
+++ src/binspector/res/icons_gui.py
@@ -6,7 +6,7 @@
 from PySide6 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x03v\
+\x00\x00\x03\x1e\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
@@ -32,39 +32,33 @@ org/2000/svg\x22\x0a  \
 e=\x22display:inlin\
 e\x22>\x0a    <path\x0a  \
      id=\x22path12\x22\
-\x0a       style=\x22d\
-isplay:inline;st\
-roke:#000000;str\
-oke-width:0;stro\
-ke-linecap:round\
-;stroke-linejoin\
-:round;paint-ord\
-er:markers strok\
-e fill\x22\x0a       d\
-=\x22M 2,0 C 0.892,\
-0 0,0.892 0,2 v \
-12 c 0,1.108 0.8\
-92,2 2,2 h 12 c \
-1.108,0 2,-0.892\
- 2,-2 V 2 C 16,0\
-.892 15.108,0 14\
-,0 Z M 5,1 H 6 V\
- 3 H 5 Z m 5,0 h\
- 1 V 3 H 10 Z M \
-1,4 H 5 V 7 H 1 \
-Z m 5,0 h 9 V 5 \
-H 6 Z m 0,2 h 9 \
-V 7 H 6 Z M 1,8 \
-h 4 v 3 H 1 Z m \
-5,0 h 9 V 9 H 6 \
-Z m 0,2 h 9 v 1 \
-H 6 Z m -5,2 h 4\
- v 3 H 1 Z m 5,0\
+\x0a       stroke=\x22\
+none\x22\x0a       fil\
+l=\x22{ButtonText}\x22\
+\x0a       d=\x22M 2,0\
+ C 0.892,0 0,0.8\
+92 0,2 v 12 c 0,\
+1.108 0.892,2 2,\
+2 h 12 c 1.108,0\
+ 2,-0.892 2,-2 V\
+ 2 C 16,0.892 15\
+.108,0 14,0 Z M \
+5,1 H 6 V 3 H 5 \
+Z m 5,0 h 1 V 3 \
+H 10 Z M 1,4 H 5\
+ V 7 H 1 Z m 5,0\
+ h 9 V 5 H 6 Z m\
+ 0,2 h 9 V 7 H 6\
+ Z M 1,8 h 4 v 3\
+ H 1 Z m 5,0 h 9\
+ V 9 H 6 Z m 0,2\
  h 9 v 1 H 6 Z m\
- 0,2 h 9 v 1 H 6\
- Z\x22 />\x0a  </g>\x0a</\
-svg>\x0a\
-\x00\x00\x04\x92\
+ -5,2 h 4 v 3 H \
+1 Z m 5,0 h 9 v \
+1 H 6 Z m 0,2 h \
+9 v 1 H 6 Z\x22 />\x0a\
+  </g>\x0a</svg>\
+\x00\x00\x04J\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
@@ -92,55 +86,50 @@ cape:groupmode=\x22\
 layer\x22\x0a     id=\x22\
 layer1\x22>\x0a    <pa\
 th\x0a       id=\x22pa\
-th1\x22\x0a       styl\
-e=\x22stroke:#00000\
-0;stroke-width:0\
-;stroke-linecap:\
-round;stroke-lin\
-ejoin:round;pain\
-t-order:markers \
-stroke fill\x22\x0a   \
-    d=\x22M 11.5,3 \
-C 11.223,3 11,3.\
-446 11,4 v 11 c \
-0,0.554 0.223,1 \
-0.5,1 0.277,0 0.\
-5,-0.446 0.5,-1 \
-V 4 C 12,3.446 1\
-1.777,3 11.5,3 Z\
- M 2,0 C 0.892,0\
- 0,0.892 0,2 v 1\
-2 c 0,1.108 0.89\
-2,2 2,2 h 9.5 C \
-11.223,16 11,15.\
-554 11,15 H 5 v \
--2 h 6 V 12 H 5 \
-v -2 h 6 V 9 H 5\
- V 7 h 6 V 6 H 5\
- V 4 h 6 C 11,3.\
-446 11.223,3 11.\
-5,3 H 11 V 1 h 1\
- V 3 H 11.5 C 11\
-.777,3 12,3.446 \
-12,4 h 3 v 2 h -\
-3 v 1 h 3 v 2 h \
--3 v 1 h 3 v 2 h\
- -3 v 1 h 3 v 2 \
-h -3 c 0,0.554 -\
-0.223,1 -0.5,1 H\
- 14 c 1.108,0 2,\
--0.892 2,-2 V 2 \
-C 16,0.892 15.10\
-8,0 14,0 Z M 4,1\
- H 5 V 3 H 4 Z M\
- 1,4 H 4 V 6 H 1\
- Z M 1,7 H 4 V 9\
- H 1 Z m 0,3 h 3\
- v 2 H 1 Z m 0,3\
- h 3 v 2 H 1 Z\x22 \
-/>\x0a  </g>\x0a</svg>\
-\x0a\
-\x00\x00\x02\xce\
+th1\x22\x0a       stro\
+ke=\x22none\x22\x0a      \
+ fill=\x22{ButtonTe\
+xt}\x22\x0a       d=\x22M\
+ 11.5,3 C 11.223\
+,3 11,3.446 11,4\
+ v 11 c 0,0.554 \
+0.223,1 0.5,1 0.\
+277,0 0.5,-0.446\
+ 0.5,-1 V 4 C 12\
+,3.446 11.777,3 \
+11.5,3 Z M 2,0 C\
+ 0.892,0 0,0.892\
+ 0,2 v 12 c 0,1.\
+108 0.892,2 2,2 \
+h 9.5 C 11.223,1\
+6 11,15.554 11,1\
+5 H 5 v -2 h 6 V\
+ 12 H 5 v -2 h 6\
+ V 9 H 5 V 7 h 6\
+ V 6 H 5 V 4 h 6\
+ C 11,3.446 11.2\
+23,3 11.5,3 H 11\
+ V 1 h 1 V 3 H 1\
+1.5 C 11.777,3 1\
+2,3.446 12,4 h 3\
+ v 2 h -3 v 1 h \
+3 v 2 h -3 v 1 h\
+ 3 v 2 h -3 v 1 \
+h 3 v 2 h -3 c 0\
+,0.554 -0.223,1 \
+-0.5,1 H 14 c 1.\
+108,0 2,-0.892 2\
+,-2 V 2 C 16,0.8\
+92 15.108,0 14,0\
+ Z M 4,1 H 5 V 3\
+ H 4 Z M 1,4 H 4\
+ V 6 H 1 Z M 1,7\
+ H 4 V 9 H 1 Z m\
+ 0,3 h 3 v 2 H 1\
+ Z m 0,3 h 3 v 2\
+ H 1 Z\x22 />\x0a  </g\
+>\x0a</svg>\x0a\
+\x00\x00\x02\x81\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
@@ -163,30 +152,26 @@ org/2000/svg\x22\x0a  \
 \x0a     id=\x22defs1\x22\
  />\x0a  <path\x0a    \
  id=\x22path5\x22\x0a    \
- style=\x22stroke:#\
-000000;stroke-wi\
-dth:0;stroke-lin\
-ecap:round;strok\
-e-linejoin:round\
-;paint-order:mar\
-kers stroke fill\
-\x22\x0a     d=\x22M 5,4 \
-C 3.892,4 3,4.89\
-2 3,6 v 8 c 0,1.\
-108 0.892,2 2,2 \
-h 9 c 1.108,0 2,\
--0.892 2,-2 V 6 \
-C 16,4.892 15.10\
-8,4 14,4 Z M 4,5\
- h 11 v 7 H 4 Z \
-M 2,0 C 0.892,0 \
-0,0.892 0,2 v 8 \
-c 0,1.108 0.892,\
-2 2,2 H 3 V 8 H \
-1 V 1 h 11 v 3 h\
- 1 V 2 C 13,0.89\
-2 12.108,0 11,0 \
-Z\x22 />\x0a</svg>\x0a\
+ stroke=\x22none\x22\x0a\x09\
+ fill=\x22{ButtonTe\
+xt}\x22\x0a     d=\x22M 5\
+,4 C 3.892,4 3,4\
+.892 3,6 v 8 c 0\
+,1.108 0.892,2 2\
+,2 h 9 c 1.108,0\
+ 2,-0.892 2,-2 V\
+ 6 C 16,4.892 15\
+.108,4 14,4 Z M \
+4,5 h 11 v 7 H 4\
+ Z M 2,0 C 0.892\
+,0 0,0.892 0,2 v\
+ 8 c 0,1.108 0.8\
+92,2 2,2 H 3 V 8\
+ H 1 V 1 h 11 v \
+3 h 1 V 2 C 13,0\
+.892 12.108,0 11\
+,0 Z\x22 />\x0a</svg>\x0a\
+\
 "
 
 qt_resource_name = b"\
@@ -219,12 +204,12 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x10\x00\x02\x00\x00\x00\x03\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00@\x00\x00\x00\x00\x00\x01\x00\x00\x03z\
-\x00\x00\x01\x9a\x14\xaaD\xf3\
+\x00\x00\x00@\x00\x00\x00\x00\x00\x01\x00\x00\x03\x22\
+\x00\x00\x01\x9a6m5\xe7\
 \x00\x00\x00\x1c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x9a\x14\xab\xdek\
-\x00\x00\x00`\x00\x00\x00\x00\x00\x01\x00\x00\x08\x10\
-\x00\x00\x01\x9a\x14\xaa\xdc\xcb\
+\x00\x00\x01\x9a6n/\xe5\
+\x00\x00\x00`\x00\x00\x00\x00\x00\x01\x00\x00\x07p\
+\x00\x00\x01\x9a6n\xff\xbe\
 "
 
 def qInitResources():
--- src/test_svg.py
+++ src/test_svg.py
@@ -1,43 +1,89 @@
 import sys
-from PySide6 import QtCore, QtSvg, QtSvgWidgets, QtGui, QtWidgets
+from os import PathLike
+from PySide6 import QtCore, QtSvg, QtGui, QtWidgets
 from binspector.res import icons_gui
 
-def palette_to_dict(palette:QtGui.QPalette) -> dict[str, QtGui.QColor]:
-	"""Pack roles and their hex values to a dict for string replacement"""
+class BSPalettedSvgIconEngine(QtGui.QIconEngine):
+	
+	def __init__(self, svg_path:PathLike, *args, **kwargs):
 
-	roles = {role.name: palette.color(role).name() for role in QtGui.QPalette.ColorRole}
-	#print(roles)
-	return roles
+		super().__init__(*args, **kwargs)
+
+		self._svg_path     = svg_path
+		self._renderer     = QtSvg.QSvgRenderer()
+		self._palette      = QtGui.QPalette()
+		self._palette_dict = self._paletteToDict(self._palette)
+		self._svg_template = QtCore.QByteArray(
+			bytes(QtCore.QResource(svg_path).data())
+		).toStdString()
+
+		self._cache:dict[int,QtGui.QPixmap] = dict()
+	
+	def clone(self) -> "BSPalettedSvgIconEngine":
+		return self.__class__(self._svg_path)
+	
+	def paint(self, painter:QtGui.QPainter, rect:QtCore.QRect, mode:QtGui.QIcon.Mode, state:QtGui.QIcon.State):
+
+		print("New render")
+		
+		self._renderer.load(self._svg_template.format_map(self._palette_dict).encode("utf-8"))
+		self._renderer.render(painter)
+
+		self.addPixmap(painter.device(), mode, state, str(self._palette_dict))
+
+	def addPixmap(self, pixmap:QtGui.QPixmap, mode:QtGui.QIcon.Mode, state:QtGui.QIcon.State, palette:QtGui.QPalette):
+
+		h = hash((pixmap.size(), mode, state, palette))
+		self._cache[h] = pixmap
+		print("added as ", h)
+	
+	def pixmap(self, size, mode, state):
+
+		h = hash((size, mode, state, str(self._palette_dict)))
+		if h in self._cache:
+			print("Return cache")
+			return self._cache[h]
+		
+		pixmap = QtGui.QPixmap(size)
+		pixmap.fill(QtCore.Qt.GlobalColor.transparent)
+		self.paint(QtGui.QPainter(pixmap), pixmap.rect(), mode, state)
+
+		return pixmap
+	
+	@QtCore.Slot(QtGui.QPalette)
+	def setPalette(self, palette:QtGui.QPalette):
+
+		self._palette      = palette
+		self._palette_dict = self._paletteToDict(palette)
+
+	@staticmethod
+	def _paletteToDict(palette:QtGui.QPalette) -> dict[str,str]:
+		return {role.name: palette.color(role).name() for role in QtGui.QPalette.ColorRole}
+	
 
 
-def replace_color(svg_path, replacements:dict|None=None) -> str:
-	"""Replace templated ColorRoles in an SVG with the provided palette colors"""
-
-	svg_data = QtCore.QResource(svg_path).data()
-	svg_string = bytes(svg_data).decode("utf-8").format_map(replacements)
-	#print(svg_string)
-	return QtCore.QByteArray(svg_string)
-
-
-ICON_RESOURCE_PATH = ":/icons/gui/view_list.svg"
+ICON_RESOURCES = ["list","frame","script"]
 
 app = QtWidgets.QApplication()
+app.setStyle("Fusion")
 
-svg_palette   = palette_to_dict(app.palette())
-svg_processed = replace_color(ICON_RESOURCE_PATH, svg_palette)
+window = QtWidgets.QWidget()
+window.setLayout(QtWidgets.QHBoxLayout())
+window.layout().setSpacing(0)
 
-#QtSvg.QSvgGenerator()
+for mode in ICON_RESOURCES:
 
-window = QtSvgWidgets.QSvgWidget()
-window.load(svg_processed)
+	icon_engine = BSPalettedSvgIconEngine(f":/icons/gui/view_{mode}.svg")
+	icon = QtGui.QIcon(icon_engine)
 
-app.paletteChanged.connect(lambda pal:
-	window.load(
-		replace_color(ICON_RESOURCE_PATH, palette_to_dict(pal))
-	)
-)
+	button = QtWidgets.QPushButton()
+	button.setIconSize(QtCore.QSize(16,16))
+	button.setIcon(icon)
 
-window.setFixedSize(QtCore.QSize(64,64))
+	app.paletteChanged.connect(icon_engine.setPalette)
+
+	window.layout().addWidget(button)
+
 window.show()
 
 sys.exit(app.exec())
