577842bf296e5357f799c596392128143cb706ad
Michael Jordan <michael@glowingpixel.com>
2025-11-02 11:23:05 -0800

    Better clip color chip drawing code

--- src/binspector/utils/drawing.py
+++ src/binspector/utils/drawing.py
@@ -1,50 +1,64 @@
 from PySide6 import QtCore, QtGui
 
 def draw_clip_color_chip(
-		painter:QtGui.QPainter,
-		canvas:QtCore.QRect,
-		clip_color:QtGui.QColor,
+		painter      :QtGui.QPainter,
+		canvas       :QtCore.QRect,
+		clip_color   :QtGui.QColor,
 		*,
-		border_color:QtGui.QColor,
-		border_width:int,
-		shadow_color:QtGui.QColor
-		):
+		border_color :QtGui.QColor|None=None,
+		border_width :int=2,
+		shadow_color :QtGui.QColor|None=None,
+		shadow_alpha :float=0.25
+	):
+	
+	border_color = border_color or QtGui.QPalette().buttonText()
+	shadow_color = shadow_color or QtGui.QPalette().shadow()
 	
 	# No border and no clip color?
 	if not clip_color.isValid() and (border_width==0 or not border_color.isValid()):
 		raise ValueError("Nothing to draw")
 	
-	painter.save()
+	# Calculate margins for stroke and shadow
+	margins = QtCore.QMargins(*([border_width * 2] * 4))
+	active_rect = canvas.marginsRemoved(margins)
 
-	# Draw clip color chip
+	# Pen and brush initial values
 	pen = painter.pen()
-	pen.setColor(border_color)
 	pen.setWidth(border_width)
-	painter.setPen(pen)
-
+	pen.setJoinStyle(QtCore.Qt.PenJoinStyle.RoundJoin)
+	pen.setStyle(QtCore.Qt.PenStyle.SolidLine)
+	
 	brush = painter.brush()
-	brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-	brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern if clip_color.isValid() else QtCore.Qt.BrushStyle.NoBrush)
+
+	painter.save()
+	#painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
+
+	# Draw shadow first I guess
+	if shadow_color.isValid() and border_width > 0:
+
+		shadow_offset = QtCore.QPoint(border_width,border_width)
+		active_rect.translate(shadow_offset)
+		
+		shadow_color.setAlphaF(shadow_alpha)
+		pen.setColor(shadow_color)
+		brush.setStyle(QtCore.Qt.BrushStyle.NoBrush)
+		
+		painter.setPen(pen)
+		painter.setBrush(brush)
+
+		painter.drawRect(active_rect)
+
+		active_rect.translate(-shadow_offset)
+
+	# Draw main clip color chip
+	pen.setColor(border_color)
+
 	brush.setColor(clip_color)
+	brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern if clip_color.isValid() else QtCore.Qt.BrushStyle.NoBrush)
+
+	painter.setPen(pen)
 	painter.setBrush(brush)
 
-	painter.drawRect(canvas)
-
-	# Draw shadow if set
-	if not shadow_color.isValid() or border_width==0:
-		painter.restore()
-		return
-	shadow_color.setAlphaF(0.25)
-	
-	canvas.translate(border_width,border_width)
-	
-	pen = painter.pen()
-	pen.setColor(shadow_color)
-	pen.setWidth(border_width)
-	painter.setPen(pen)
-
-	painter.setBrush(QtGui.QBrush(QtCore.Qt.BrushStyle.NoBrush))
-
-	painter.drawRect(canvas)
+	painter.drawRect(active_rect)
 
 	painter.restore()
