326cd79cf679b8a3e3e2f47539db763e581dbfd6
Michael Jordan <michael@glowingpixel.com>
2025-11-09 10:39:41 -0800

    Ugly bash

--- assets/qrc/make_res.sh
+++ assets/qrc/make_res.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+
 for f in *.qrc; do
 	pyside6-rcc "$f" -o "../../src/binspector/res/${f%.qrc}.py";
 done;
--- assets/translations/prep_translations.sh
+++ assets/translations/prep_translations.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+
 shopt -s globstar
 pyside6-lupdate ../../src/**/*.py -ts bs.zh_CN.ts
