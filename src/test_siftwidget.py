40b620673ff5dba67ab9a96ed11282986e5d85c2
Michael Jordan <michael@glowingpixel.com>
2025-11-13 15:21:16 -0800

    goodetest

--- src/test_siftwidget.py
+++ src/test_siftwidget.py
@@ -1,3 +1,4 @@
+import avb, avbutils
 from PySide6 import QtCore, QtWidgets
 from binspector.widgets import siftwidget
 
@@ -22,5 +23,21 @@ wnd.setFixedHeight(geo.height())
 
 wnd.btn_dialog.accepted.connect(app.quit)
 wnd.btn_dialog.rejected.connect(app.quit)
+wnd.btn_dialog.button(QtWidgets.QDialogButtonBox.StandardButton.Apply).clicked.connect(lambda: print(wnd.siftOptions()))
+
+import sys
+with avb.open(sys.argv[1]) as bin_file:
+	bin_view = bin_file.content.view_setting
+	wnd.setBinView(bin_view)
+
+options = []
+
+options.append(siftwidget.BSSiftOption(
+	sift_method=avbutils.bins.BinSiftMethod.MATCHES_EXACTLY,
+	sift_text="Poopy",
+	sift_column="Name",
+))
+
+wnd.setSiftOptions(options)
 
 app.exec()
