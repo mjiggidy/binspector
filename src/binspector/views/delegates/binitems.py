d152d9a425fc98467281beaade323653386e99f6
Michael Jordan <michael@glowingpixel.com>
2025-11-17 13:47:53 -0800

    Fix icon aspect ratio business (turns out I'm dumb)

--- src/binspector/views/delegates/binitems.py
+++ src/binspector/views/delegates/binitems.py
@@ -26,6 +26,7 @@ class BSGenericItemDelegate(QtWidgets.QStyledItemDelegate):
 		"""Set padding around individual items"""
 
 		if self._padding != padding:
+
 			self._padding = padding
 
 			# NOTE: Binding to sizeHintChanged here to trigger redraw
@@ -36,9 +37,6 @@ class BSGenericItemDelegate(QtWidgets.QStyledItemDelegate):
 	def activeRectFromRect(self, rect:QtCore.QRect) -> QtCore.QRect:
 		"""The active area without padding and such"""
 
-		#return rect
-		#print(self._padding)
-		#print(rect.marginsRemoved(self._padding))
 		return rect.marginsRemoved(self._padding)
 	
 	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionViewItem, index:QtCore.QModelIndex):
@@ -93,7 +91,10 @@ class BSIconLookupItemDelegate(BSGenericItemDelegate):
 	def sizeWithAspectRatio(self, rect:QtCore.QSize) -> QtCore.QSize:
 		"""Over-engineering?  Probably.  Return a size corrected for aspect ratio with priority given to height."""
 
-		return QtCore.QSize(rect.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()), rect.height())
+		return QtCore.QSize(
+			rect.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()),
+			rect.height()
+		)
 
 	def sizeHint(self, option:QtWidgets.QStyleOption, index:QtCore.QModelIndex) -> QtCore.QSize:
 		"""Return aspect ratio-corrected width x original height"""
@@ -105,27 +106,35 @@ class BSIconLookupItemDelegate(BSGenericItemDelegate):
 	
 	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionViewItem, index:QtCore.QModelIndex):
 
-		opt = QtWidgets.QStyleOptionViewItem(option)
-		self.initStyleOption(opt, index)
-		style = opt.widget.style() if opt.widget else QtWidgets.QApplication.style()
+		opt_styled = QtWidgets.QStyleOptionViewItem(option)
+		self.initStyleOption(opt_styled, index)
+		style = opt_styled.widget.style() if opt_styled.widget else QtWidgets.QApplication.style()
 		
 		user_data = index.data(QtCore.Qt.ItemDataRole.DecorationRole)
 		icon      = self._icon_provider.getIcon(user_data)
 
 		# Center, size and shape the canvas QRect
-		canvas_active = self.activeRectFromRect(opt.rect)
-		canvas_active.setWidth(
-			min(
-				canvas_active.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()),                # Full aspect ratio, or...
-				max(canvas_active.height(), canvas_active.width() - self._padding.left() - self._padding.right()) # ...clamp to smaller width
-			)
-		)
-		canvas_active.moveCenter(opt.rect.center())
+		canvas_active = self.activeRectFromRect(opt_styled.rect)
+		
+		# Based on the active canvas area, bind the width to
+		# Min: Same as height (square)
+		# Max: Aspect ratio (w * w/h)
+		w_min = canvas_active.height()
+		w_max = canvas_active.height() * (self._aspect_ratio.width() / self._aspect_ratio.height())
+		if canvas_active.width() < canvas_active.height():
+			w_active = w_min
+		elif canvas_active.width() > w_max:
+			w_active = w_max
+		else:
+			w_active = canvas_active.width()
+		
+		canvas_active.setWidth(w_active)
+		canvas_active.moveCenter(opt_styled.rect.center())
 		
 		painter.save()
 		
 		try:
-			style.drawPrimitive(QtWidgets.QStyle.PrimitiveElement.PE_PanelItemViewItem, opt, painter, opt.widget)
+			style.drawPrimitive(QtWidgets.QStyle.PrimitiveElement.PE_PanelItemViewItem, opt_styled, painter, opt_styled.widget)
 			icon.paint(
 				painter,
 				canvas_active
@@ -134,64 +143,6 @@ class BSIconLookupItemDelegate(BSGenericItemDelegate):
 		except Exception as e:
 			logging.getLogger(__name__).error("Error drawing icon: %s", e)
 		
-		painter.restore()
-
-class LBClipColorItemDelegate(BSGenericItemDelegate):
-	"""Draw a clip color chip(?) to a give painter"""
-
-	def __init__(self, *args, aspect_ratio:QtCore.QSize|None=None, border_width:int=1, **kwargs):
-
-		raise DeprecationWarning("Use BSIconLookupItemDelegate instead there, hoss")
-
-		super().__init__(*args, **kwargs)
-
-		self._aspect_ratio = aspect_ratio or QtCore.QSize(4,3)
-		self._border_width = border_width
-
-	def sizeHint(self, option:QtWidgets.QStyleOption, index:QtCore.QModelIndex) -> QtCore.QSize:
-
-		orig = super().sizeHint(option, index)
-
-		# Return aspect ratio-corrected width x original height
-		return QtCore.QSize(orig.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()), orig.height())
-
-	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionViewItem, index:QtCore.QModelIndex):
-
-		super().paint(painter, option, index)
-
-		# "No clip color" should at least be an invalid `QtCore.QColor`
-		clip_color = index.data(QtCore.Qt.ItemDataRole.UserRole)
-		if not isinstance(clip_color, QtGui.QColor):
-			logging.getLogger(__name__).error("Skipping clip color delegate because UserRole was not QColor (got %s)", type(clip_color).__name__)
-			return
-		
-		# Center, size and shape the canvas QRect
-		canvas_active = self.activeRectFromRect(option.rect)
-		canvas_active.setWidth(
-			min(
-				canvas_active.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()),        # Full aspect ratio, or...
-				max(canvas_active.height(), canvas_active.width() - self._padding.left() - self._padding.right() + self._border_width) # ...clamp to smaller width
-			)
-		)
-		canvas_active.moveCenter(option.rect.center())
-		
-		painter.save()
-
-		#painter.fillRect(canvas_active, QtGui.QColor("Black"))
-
-		try:
-			drawing.draw_clip_color_chip(
-				painter=painter,
-				canvas=canvas_active,
-				clip_color=clip_color,
-				border_color=option.palette.color(QtGui.QPalette.ColorRole.WindowText),
-				border_width=self._border_width,
-				shadow_color=option.palette.color(QtGui.QPalette.ColorRole.Shadow),
-			)
-		except Exception as e:
-			print(e)
-			logging.getLogger(__name__).error("Error drawing clip color delegate: %s", e)
-
 		painter.restore()
 
 class LBTimecodeItemDelegate(BSGenericItemDelegate):
@@ -204,3 +155,61 @@ class LBTimecodeItemDelegate(BSGenericItemDelegate):
 	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOption, index:QtCore.QModelIndex):
 		
 		super().paint(painter, option, index)
+
+#ass LBClipColorItemDelegate(BSGenericItemDelegate):
+#	"""Draw a clip color chip(?) to a give painter"""
+#
+#	def __init__(self, *args, aspect_ratio:QtCore.QSize|None=None, border_width:int=1, **kwargs):
+#
+#		raise DeprecationWarning("Use BSIconLookupItemDelegate instead there, hoss")
+#
+#		super().__init__(*args, **kwargs)
+#
+#		self._aspect_ratio = aspect_ratio or QtCore.QSize(4,3)
+#		self._border_width = border_width
+#
+#	def sizeHint(self, option:QtWidgets.QStyleOption, index:QtCore.QModelIndex) -> QtCore.QSize:
+#
+#		orig = super().sizeHint(option, index)
+#
+#		# Return aspect ratio-corrected width x original height
+#		return QtCore.QSize(orig.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()), orig.height())
+#
+#	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionViewItem, index:QtCore.QModelIndex):
+#
+#		super().paint(painter, option, index)
+#
+#		# "No clip color" should at least be an invalid `QtCore.QColor`
+#		clip_color = index.data(QtCore.Qt.ItemDataRole.UserRole)
+#		if not isinstance(clip_color, QtGui.QColor):
+#			logging.getLogger(__name__).error("Skipping clip color delegate because UserRole was not QColor (got %s)", type(clip_color).__name__)
+#			return
+#		
+#		# Center, size and shape the canvas QRect
+#		canvas_active = self.activeRectFromRect(option.rect)
+#		canvas_active.setWidth(
+#			min(
+#				canvas_active.height() * (self._aspect_ratio.width()/self._aspect_ratio.height()),        # Full aspect ratio, or...
+#				max(canvas_active.height(), canvas_active.width() - self._padding.left() - self._padding.right() + self._border_width) # ...clamp to smaller width
+#			)
+#		)
+#		canvas_active.moveCenter(option.rect.center())
+#		
+#		painter.save()
+#
+#		#painter.fillRect(canvas_active, QtGui.QColor("Black"))
+#
+#		try:
+#			drawing.draw_clip_color_chip(
+#				painter=painter,
+#				canvas=canvas_active,
+#				clip_color=clip_color,
+#				border_color=option.palette.color(QtGui.QPalette.ColorRole.WindowText),
+#				border_width=self._border_width,
+#				shadow_color=option.palette.color(QtGui.QPalette.ColorRole.Shadow),
+#			)
+#		except Exception as e:
+#			print(e)
+#			logging.getLogger(__name__).error("Error drawing clip color delegate: %s", e)
+#
+#		painter.restore()
