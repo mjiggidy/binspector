9d08e703b9476079740cace9edcafe187134b506
Michael Jordan <michael@glowingpixel.com>
2025-11-22 15:29:10 -0800

    Rough coordinates

--- src/binspector/views/binframeview.py
+++ src/binspector/views/binframeview.py
@@ -12,16 +12,18 @@ class BSBinFrameView(QtWidgets.QGraphicsView):
 		self.setInteractive(True)
 		self.setDragMode(QtWidgets.QGraphicsView.DragMode.RubberBandDrag)
 		self.setViewportUpdateMode(QtWidgets.QGraphicsView.ViewportUpdateMode.FullViewportUpdate)
+
+		self._current_zoom = 1
 	
 	def drawBackground(self, painter:QtGui.QPainter, rect:QtCore.QRectF):
-		
-		TEMP_SCALE = 12
-		
-		GRID_WIDTH = 16 * TEMP_SCALE
-		GRID_HEIGHT = 9 * TEMP_SCALE
-		
 
-		DIVISIONS = 3
+		raise DeprecationWarning("WHAT")
+		
+		BASE_MAGNIFICATION = 12
+		
+		GRID_WIDTH         = 16 * BASE_MAGNIFICATION
+		GRID_HEIGHT        =  9 * BASE_MAGNIFICATION
+		GRID_DIVISIONS     =  3
 
 		super().drawBackground(painter, rect)
 
@@ -35,6 +37,9 @@ class BSBinFrameView(QtWidgets.QGraphicsView):
 					QtCore.QPoint(x, rect.top()),
 					QtCore.QPoint(x, rect.bottom())
 				))
+
+				print("HEY YOOHO HO")
+				#painter.drawText(QtCore.QPoint(x, 50), f"{x},0")
 
 
 		for y in range(int(rect.top()), int(rect.bottom())+1):
@@ -54,43 +59,61 @@ class BSBinFrameView(QtWidgets.QGraphicsView):
 	def setZoom(self, zoom_level:int):
 		#print("I SET ZOOM", zoom_level)
 
-		zoom_level = float(zoom_level) #/ float(4)
-		
-		t = QtGui.QTransform()
-		t.scale(zoom_level, zoom_level)
-		self.setTransform(t)
+		if zoom_level != self._current_zoom:
+			
+			import logging
+			logging.getLogger(__name__).debug("Setting zoom level to %s", zoom_level)
 
-		self.sig_scale_changed.emit(zoom_level)
+			zoom_level = float(zoom_level) #/ float(4)
+			self._current_zoom = zoom_level
+
+			t = QtGui.QTransform()
+			t.scale(zoom_level, zoom_level)
+			self.setTransform(t)
+
+			self.sig_scale_changed.emit(zoom_level)
 	
 	def drawBackground(self, painter:QtGui.QPainter, rect:QtCore.QRectF):
 
 		GRID_DIVISIONS = 3
-		GRID_UNIT_RECT = QtCore.QRectF(
-			QtCore.QPoint(0,0),
-			QtCore.QPoint(18,12)
-		)
+		GRID_UNIT_SIZE = QtCore.QSizeF(18,12)
 
 		pen_boundary = QtGui.QPen()
 		pen_boundary.setStyle(QtCore.Qt.PenStyle.SolidLine)
 		pen_boundary.setCosmetic(True)
 		pen_boundary.setWidth(1)
+		pen_boundary.setColor(self.parentWidget().palette().shadow().color())
 
 		pen_division = QtGui.QPen()
 		pen_division.setStyle(QtCore.Qt.PenStyle.DashLine)
 		pen_division.setCosmetic(True)
 		pen_division.setWidth(1)
+		pen_division.setColor(self.parentWidget().palette().shadow().color())
 
-		
 		super().drawBackground(painter, rect)
 
 		painter.save()
 
+		# Setup stuff for ruler
+		
+		coord_font = self.font()
+		coord_font.setPointSizeF(coord_font.pointSizeF()/(self._current_zoom))
+		painter.setFont(coord_font)
+		
+		#import logging
+		#logging.getLogger(__name__).debug("Set font size to %s px", coord_font.pointSizeF())
+
+		mapped_viewport_rect = self.mapToScene(self.viewport().rect()).boundingRect()
+
+		y_top = mapped_viewport_rect.top()
+		x_left = mapped_viewport_rect.left()
+
 		for x in range(round(rect.left()), round(rect.right())+1):
 
-			if x % (GRID_UNIT_RECT.width() // GRID_DIVISIONS):
+			if x % (GRID_UNIT_SIZE.width() // GRID_DIVISIONS):
 				continue
 
-			if x % (GRID_UNIT_RECT.width()) == 0:
+			if x % (GRID_UNIT_SIZE.width()) == 0:
 				painter.setPen(pen_boundary)
 			else:
 				painter.setPen(pen_division)
@@ -99,13 +122,15 @@ class BSBinFrameView(QtWidgets.QGraphicsView):
 				QtCore.QPoint(x, rect.top()),
 				QtCore.QPoint(x, rect.bottom())
 			))
+
+			painter.drawText(QtCore.QPointF(x, float(y_top) - (float(y_top) - float(y_top + GRID_UNIT_SIZE.height()))-11), str(int(x)))
 		
 		for y in range(int(rect.top()), int(rect.bottom())+1):
 			
-			if y % (GRID_UNIT_RECT.height() // GRID_DIVISIONS):
+			if y % (GRID_UNIT_SIZE.height() // GRID_DIVISIONS):
 				continue
 
-			if y % (GRID_UNIT_RECT.height()) == 0:
+			if y % (GRID_UNIT_SIZE.height()) == 0:
 				painter.setPen(pen_boundary)
 			else:
 				painter.setPen(pen_division)
@@ -115,5 +140,6 @@ class BSBinFrameView(QtWidgets.QGraphicsView):
 				QtCore.QPoint(rect.right(), y),
 			))
 
+			painter.drawText(QtCore.QPoint(float(x_left) - (float(x_left) - float(x_left + GRID_UNIT_SIZE.width())+18), y), str(int(y)))
 
 		painter.restore()
