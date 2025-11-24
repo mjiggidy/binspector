aa257308d36a7059eec6c6ae9f6749f175941fcd
Michael Jordan <michael@glowingpixel.com>
2025-11-24 13:18:58 -0800

    Simple PInch

---
+++ src/binspector/managers/eventfilters.py
@@ -0,0 +1,68 @@
+"""
+Event filters to handle particular system events 
+in a good and nice way that is good and nice
+"""
+
+from PySide6 import QtCore
+
+class BSPinchPanEventFilter(QtCore.QObject):
+	"""Handle pinch 'n' pan multitouch events"""
+	
+	# I peench
+
+	GESTURE_SAMPLE_TIMEOUT_MS = 40
+	"""Sample rate (milliseconds) to avoid signal storm"""
+
+	sig_user_started_gesture  = QtCore.Signal(object)
+	"""I peench"""
+	
+	sig_user_is_pinching    = QtCore.Signal(float)
+	"""User did a pinchy"""
+
+	sig_user_finished_gesture = QtCore.Signal(object)
+	"""User has tired of the pinch"""
+
+	def __init__(self, *args, **kwargs):
+
+		super().__init__(*args, **kwargs)
+
+		self._pinch_response_curve = QtCore.QEasingCurve()
+		self._accumulate = 0
+		self._tracked_gesture_count = 0
+
+		self._pinch_response_curve.setType(QtCore.QEasingCurve.Type.InOutQuad)
+	
+	def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent):
+
+		if not event.type() == QtCore.QEvent.Type.NativeGesture:
+			return super().eventFilter(watched, event)
+
+		# If Begin Native Gesture
+		if event.gestureType() == QtCore.Qt.NativeGestureType.BeginNativeGesture:
+			
+			self._tracked_gesture_count += 1
+			self.sig_user_started_gesture.emit(event.gestureType())
+		
+		# If End Native Gesture
+		elif event.gestureType() == QtCore.Qt.NativeGestureType.EndNativeGesture:
+			
+			self._tracked_gesture_count -= 1
+			
+
+			if not self._tracked_gesture_count:
+
+				self.sig_user_finished_gesture.emit(event.gestureType())
+				self._accumulate = 0
+		
+		# If Actively Zooming
+		elif event.gestureType() == QtCore.Qt.NativeGestureType.ZoomNativeGesture:
+		
+			self._accumulate += event.value()
+			self.reportPinchZoom(self._accumulate)
+
+		return super().eventFilter(watched, event)
+	
+	@QtCore.Slot()
+	def reportPinchZoom(self, zoom_delta:float):
+
+		self.sig_user_is_pinching.emit(zoom_delta)
