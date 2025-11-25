e7a6d9b4347316a5d5e64823dfa76363fccdf6ce
Michael Jordan <michael@glowingpixel.com>
2025-11-25 13:06:30 -0800

    Pinch cleanup, pan touch works but no wheel

--- src/binspector/managers/eventfilters.py
+++ src/binspector/managers/eventfilters.py
@@ -9,13 +9,13 @@ from PySide6 import QtCore, QtGui
 class BSPinchEventFilter(QtCore.QObject):
 	"""Handle if the user gets pinchy"""
 
-	sig_user_started_gesture  = QtCore.Signal()
+	sig_user_pinch_started  = QtCore.Signal()
 	"""The user has began to pinch"""
 	
-	sig_user_is_pinching    = QtCore.Signal(float, float)
+	sig_user_pinch_moved    = QtCore.Signal(float, float)
 	"""User is doing a pinchy (`delta since last:float`, `accumulated pinch:float`)"""
 
-	sig_user_finished_gesture = QtCore.Signal()
+	sig_user_pinch_finished = QtCore.Signal()
 	"""User has tired of the pinch"""
 
 	def __init__(self, *args, **kwargs):
@@ -25,7 +25,8 @@ class BSPinchEventFilter(QtCore.QObject):
 		self._accumulated_scale     = 0
 		self._tracked_gesture_count = 0
 	
-	def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent):
+	def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent) -> bool:
+		"""Handle pinch events via `QtGui.QNativeGestureEvent` thingies"""
 
 		if event.type() != QtCore.QEvent.Type.NativeGesture:
 			return False
@@ -33,42 +34,43 @@ class BSPinchEventFilter(QtCore.QObject):
 		# If Begin Native Gesture
 		if event.gestureType() == QtCore.Qt.NativeGestureType.BeginNativeGesture:
 			
-			self.reportPinchBegan()
+			self.reportPinchStarted()
 			return True
 			
 		# If Actively Zooming
 		elif event.gestureType() == QtCore.Qt.NativeGestureType.ZoomNativeGesture:
 			
-			self.reportPinchUpdated(event)
+			self.reportPinchChanged(event)
 			return True
 
 		# If End Native Gesture
 		elif event.gestureType() == QtCore.Qt.NativeGestureType.EndNativeGesture:
 			
-			self.reportPinchEnded()
+			self.reportPinchFinished()
 			return True
 		
 		return False
 	
 	@QtCore.Slot()
-	def reportPinchBegan(self):
-		"""User began The Pinch"""
+	def reportPinchStarted(self):
+		"""User is getting pinchy"""
 
 		self._tracked_gesture_count += 1
 		logging.getLogger(__name__).debug("Begin gesture, active_gestures=%s", self._tracked_gesture_count)
 		
 		if self._tracked_gesture_count == 1:
-			self.sig_user_started_gesture.emit()
+			self.sig_user_pinch_started.emit()
 
 	@QtCore.Slot(object)
-	def reportPinchUpdated(self, pinch_event:QtGui.QNativeGestureEvent):
+	def reportPinchChanged(self, pinch_event:QtGui.QNativeGestureEvent):
 		"""Report the latest accumulated value of The Pinch"""
 
 		self._accumulated_scale += pinch_event.value()
-		self.sig_user_is_pinching.emit(pinch_event.value(), self._accumulated_scale)
+		self.sig_user_pinch_moved.emit(pinch_event.value(), self._accumulated_scale)
 
 	@QtCore.Slot()
-	def reportPinchEnded(self):
+	def reportPinchFinished(self):
+		"""User has finished with his little pinches"""
 
 		self._tracked_gesture_count -= 1
 		logging.getLogger(__name__).debug("End gesture, active_gestures=%s",self._tracked_gesture_count)
@@ -76,7 +78,7 @@ class BSPinchEventFilter(QtCore.QObject):
 		if self._tracked_gesture_count < 1:
 			
 			self._reset()
-			self.sig_user_finished_gesture.emit()
+			self.sig_user_pinch_finished.emit()
 	
 	def _reset(self):
 		"""Reset tracking values for next pinch"""
@@ -85,3 +87,109 @@ class BSPinchEventFilter(QtCore.QObject):
 		self._accumulated_scale = 0
 		
 		logging.getLogger(__name__).debug("Pinch reset")
+
+
+class BSPanEventFilter(QtCore.QObject):
+	"""Handle user panning via mouse wheel or multitouch trackpad"""
+
+	sig_user_pan_started  = QtCore.Signal()
+	"""User began the pan"""
+
+	sig_user_pan_moved    = QtCore.Signal(QtCore.QPoint, QtCore.QPoint)
+	"""User has panned (`delta:QtCore.QPoint`, `accumulated:QtCore.QPoint`)"""
+
+	sig_user_pan_finished = QtCore.Signal()
+	"""User has tired of the pan"""
+
+	def __init__(self, *args, **kwargs):
+
+		super().__init__(*args, **kwargs)
+
+		self._panning_active = False
+		self._accumulated_pan = QtCore.QPoint(0,0)
+		self._last_start_pos  = QtCore.QPoint(0,0)
+
+	def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent) -> bool:
+		
+		if event.type() == QtCore.QEvent.Type.Wheel and event.deviceType() == QtGui.QInputDevice.DeviceType.TouchPad:
+			
+			if event.phase() == QtCore.Qt.ScrollPhase.ScrollUpdate:
+				self.reportPanChanged(event, event.angleDelta())
+				return True
+			
+			elif event.phase() == QtCore.Qt.ScrollPhase.ScrollEnd:
+				self.reportPanFinished(event)
+				return True
+			
+			return False
+		
+		elif event.type() == QtCore.QEvent.Type.MouseButtonPress and event.button() == QtCore.Qt.MouseButton.MiddleButton:
+			
+			self.reportPanStarted(event)
+			return True
+
+		elif event.type() == QtCore.QEvent.Type.MouseMove and self._panning_active:
+
+			delta = QtCore.QPoint(self._last_start_pos)
+			
+			print("Start", delta)
+			
+			delta = event.position().toPoint() - self._last_start_pos
+			
+			print("After",delta)
+			self._last_start_pos = delta
+			#self._last_start_pos = QtCore.QPoint(delta)
+			#print(self._last_start_pos)
+			return True
+			delta_pos = event.position().toPoint() - self._last_start_pos
+			self._last_start_pos = QtCore.QPoint(delta_pos)
+
+			print(delta_pos)
+
+			self.reportPanChanged(event, delta_pos)
+
+		elif event.type() == QtCore.QEvent.Type.MouseButtonRelease and event.button() == QtCore.Qt.MouseButton.MiddleButton:
+
+			self.reportPanFinished(event)
+			return True
+		#else:
+		#	print(event)
+		
+		return False
+	
+	def reportPanStarted(self, pan_event:QtGui.QMouseEvent):
+		"""User started to pan"""
+
+		if self._panning_active:
+			return
+		
+		self._last_start_pos = pan_event.position().toPoint()
+		self._panning_active = True
+		logging.getLogger(__name__).debug("Pan started (device=%s, start_pos=%s)", pan_event.device(), self._last_start_pos)
+		self.sig_user_pan_started.emit()
+
+
+	def reportPanChanged(self, pan_event:QtGui.QMouseEvent, pan_delta:QtCore.QPoint):
+		"""User is panning around"""
+
+		if not self._panning_active:
+			self.reportPanStarted(pan_event)
+
+		self._accumulated_pan += pan_delta
+
+
+		self.sig_user_pan_moved.emit(pan_delta, self._accumulated_pan)
+	
+	def reportPanFinished(self, pan_event:QtGui.QMouseEvent):
+		"""User is no longer panning"""
+
+		if not self._panning_active:
+			return
+		
+		self._panning_active = False
+		self._accumulated_pan = QtCore.QPoint(0,0)
+		self._last_start_pos = QtCore.QPoint(0,0)
+
+		logging.getLogger(__name__).debug("Pan finished (device=%s)", pan_event.device())
+
+		self.sig_user_pan_finished.emit()
