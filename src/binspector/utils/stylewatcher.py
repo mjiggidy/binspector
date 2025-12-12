39faf053336239ed1da1c00c6b0e1e878519644c
Michael Jordan <michael@glowingpixel.com>
2025-12-12 12:49:32 -0800

    Style watcher... eh

---
+++ src/binspector/utils/stylewatcher.py
@@ -0,0 +1,17 @@
+from PySide6 import QtCore, QtGui, QtWidgets
+
+class BSWidgetStyleEventFilter(QtCore.QObject):
+	"""Watch a widget for visual style updates"""
+
+	sig_palette_changed = QtCore.Signal(QtGui.QPalette)
+	sig_font_changed    = QtCore.Signal(QtGui.QFont)
+
+	def eventFilter(self, watched:QtWidgets.QWidget, event:QtCore.QEvent) -> bool:
+
+		if event.type() == QtCore.QEvent.Type.PaletteChange:
+			self.sig_palette_changed.emit(watched.palette())
+		
+		elif event.type() == QtCore.QEvent.Type.FontChange:
+			self.sig_font_changed.emit(watched.font())
+
+		return super().eventFilter(watched, event)
