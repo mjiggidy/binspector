4a14f4d2f11b49e9acdac892d153b04cbe912437
Michael Jordan <michael@glowingpixel.com>
2025-12-12 17:19:49 -0800

    liltweak

--- src/binspector/views/logtreeview.py
+++ src/binspector/views/logtreeview.py
@@ -1,7 +1,8 @@
 from PySide6 import QtWidgets
+from . import treeview
 from ..models import logmodels
 
-class BSLogTreeView(QtWidgets.QTreeView):
+class BSLogTreeView(treeview.BSTreeViewBase):
 	"""A TreeView for viewin' trees. And logs.  Logs from trees."""
 
 	def __init__(self, log_model:logmodels.BSLogDataModel|None=None, *args, **kwargs):
