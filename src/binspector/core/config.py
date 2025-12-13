c650fff1f564f9c4fbd2cf565334e4802cf66b57
Michael Jordan <michael@glowingpixel.com>
2025-12-12 16:43:05 -0800

    Notes

--- src/binspector/core/config.py
+++ src/binspector/core/config.py
@@ -6,11 +6,16 @@ class BSListViewConfig:
 	"""Default padding inside view item"""
 
 	DEFAULT_SELECTION_BEHAVIOR = QtWidgets.QTreeView.SelectionBehavior.SelectRows
+	"""Default selection behavior (select rows, columns, or items)"""
+
 	DEFAULT_SELECTION_MODE     = QtWidgets.QTreeView.SelectionMode    .ExtendedSelection
+	"""Default selection mode (continuous, extended, etc)"""
 
 	ALLOW_KEEP_CURRENT_SELECTION_BETWEEN_MODES = False
-	"""Actually allow `keep_current_selection` bool argument to take effect"""
+	"""Maintain selected items when changing between selection modes (rows/colums/items)"""
+	# NOTE: There's a bool at work in the logic as well -- both'll need to be True
 
+	# NOT USED? =====
 	SELECTION_BEHAVIOR_MODIFIER_KEY:QtCore.Qt.KeyboardModifier = QtCore.Qt.KeyboardModifier.AltModifier # NOT USED?
 	BINVIEW_COLUMN_WIDTH_ADJUST:int = 64	# NOT USED?
 	"""Adjust binview-specified column widths for better fit"""
