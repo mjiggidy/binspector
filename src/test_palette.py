4f2559935ec0c4f64c40f229a69c1d5d87816408
Michael Jordan <michael@glowingpixel.com>
2025-12-18 10:26:52 -0800

    Test

--- src/test_palette.py
+++ src/test_palette.py
@@ -1,35 +1,80 @@
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 
+ORDERED_COLOR_ROLES = [
+	QtGui.QPalette.ColorRole.Base,
+	QtGui.QPalette.ColorRole.AlternateBase,
+	QtGui.QPalette.ColorRole.Window,
+	QtGui.QPalette.ColorRole.ToolTipBase,
+
+	QtGui.QPalette.ColorRole.Text,
+	QtGui.QPalette.ColorRole.WindowText,
+	QtGui.QPalette.ColorRole.ToolTipText,
+	QtGui.QPalette.ColorRole.PlaceholderText,
+	QtGui.QPalette.ColorRole.ButtonText,
+	QtGui.QPalette.ColorRole.BrightText,
+
+	QtGui.QPalette.ColorRole.Light,
+	QtGui.QPalette.ColorRole.Midlight,
+	QtGui.QPalette.ColorRole.Button,
+	QtGui.QPalette.ColorRole.Mid,
+	QtGui.QPalette.ColorRole.Dark,
+	QtGui.QPalette.ColorRole.Shadow,
+
+	QtGui.QPalette.ColorRole.Accent,
+	QtGui.QPalette.ColorRole.Highlight,
+	QtGui.QPalette.ColorRole.HighlightedText,
+
+	QtGui.QPalette.ColorRole.Link,
+	QtGui.QPalette.ColorRole.LinkVisited,
+]
+
 class ColorPaletteLabel(QtWidgets.QLabel):
 
-	def __init__(self, color_role:QtGui.QPalette.ColorRole):
+	def __init__(self, color_role:QtGui.QPalette.ColorRole, color_group:QtGui.QPalette.ColorGroup=QtGui.QPalette.ColorGroup.Active):
 
 		super().__init__()
 
-		self._color_role = color_role
+		self._color_role  = color_role
+		self._color_group = color_group
 
 		self.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
 		self.setAutoFillBackground(True)
 
-		self.setPalette(self.palette())
+		self._updateColor()
 	
 	def sizeHint(self) -> QtCore.QSize:
-		return QtCore.QSize(128,64)/2
+		return QtCore.QSize(128,64) / 2
 	
-	@QtCore.Slot(QtGui.QPalette)
-	def setPalette(self, palette:QtGui.QPalette):
+	@QtCore.Slot()
+	def _updateColor(self):
 
-
-		color = self.palette().color(self._color_role)
-
+		palette = self.palette()
+		
+		color = self.palette().color(self._color_group, self._color_role)
 		palette.setColor(QtGui.QPalette.ColorRole.Window, color)
-		palette.setColor(QtGui.QPalette.ColorRole.Base, color)
-		palette.setColor(QtGui.QPalette.ColorRole.Accent, color)
 
-		self.setText(color.name())
+		print(palette)
 
-		return super().setPalette(palette)
+		self.setPalette(palette)
+		self.setText(f"R: {color.red()}  G: {color.green()}  B: {color.blue()} A: {color.alpha()}")
+		self.setToolTip(color.name())
+	
+	@QtCore.Slot(QtGui.QPalette.ColorRole)
+	def setColorRole(self, role:QtGui.QPalette.ColorRole):
+
+		self._color_role = role
+		self._updateColor()
+	
+	@QtCore.Slot(QtGui.QPalette.ColorGroup)
+	def setColorGroup(self, group:QtGui.QPalette.ColorGroup):
+
+		self._color_group = group
+		self._updateColor()
+
+	def setPalette(self, arg__1):
+		super().setPalette(arg__1)
+		self._updateColor()
 
 class MyKewlPaletteViewer(QtWidgets.QWidget):
 
@@ -39,12 +84,21 @@ class MyKewlPaletteViewer(QtWidgets.QWidget):
 
 		self.setLayout(QtWidgets.QGridLayout())
 
-		for row, color_role in enumerate(QtGui.QPalette.ColorRole):
+		self._cmb_colorgroups = QtWidgets.QComboBox()
+		
+		for group in QtGui.QPalette.ColorGroup:
+			self._cmb_colorgroups.addItem(group.name, group)
 
-			self.layout().addWidget(QtWidgets.QLabel(color_role.name), row, 0)
-			self.layout().addWidget(ColorPaletteLabel(color_role), row, 1)
+		self.layout().addWidget(self._cmb_colorgroups, 0, 0, 1, 2)
 
+		for row, color_role in enumerate(ORDERED_COLOR_ROLES):
 
+			swatch = ColorPaletteLabel(color_role)
+			self._cmb_colorgroups.activated.connect(lambda: swatch.setColorGroup(self._cmb_colorgroups.currentData()))
+
+			self.layout().addWidget(QtWidgets.QLabel(color_role.name), row+1, 0)
+			self.layout().addWidget(swatch, row+1, 1)
+	
 if __name__ == "__main__":
 
 	app = QtWidgets.QApplication()
