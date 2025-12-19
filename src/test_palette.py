93526dd06421d15b9f3c42c074305af8fb35a1ba
Michael Jordan <michael@glowingpixel.com>
2025-12-19 09:48:53 -0800

    Palette tweaks

--- src/test_palette.py
+++ src/test_palette.py
@@ -1,5 +1,8 @@
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
+from binspector.utils import stylewatcher, palettes
+
+from binspector.binwidget import binwidget
 
 ORDERED_COLOR_ROLES = [
 	QtGui.QPalette.ColorRole.Base,
@@ -35,8 +38,12 @@ class ColorPaletteLabel(QtWidgets.QLabel):
 
 		super().__init__()
 
-		self._color_role  = color_role
-		self._color_group = color_group
+		self._color_role   = color_role
+		self._color_group  = color_group
+
+		self._stylewatcher = stylewatcher.BSWidgetStyleEventFilter(parent=self)
+		self._stylewatcher.sig_palette_changed.connect(self._updateColor)
+		self.installEventFilter(self._stylewatcher)
 
 		self.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
 		self.setAutoFillBackground(True)
@@ -50,15 +57,14 @@ class ColorPaletteLabel(QtWidgets.QLabel):
 	def _updateColor(self):
 
 		palette = self.palette()
-		
-		color = self.palette().color(self._color_group, self._color_role)
+		color   = self.palette().color(self._color_group, self._color_role)
 		palette.setColor(QtGui.QPalette.ColorRole.Window, color)
-
-		print(palette)
-
+		
 		self.setPalette(palette)
 		self.setText(f"R: {color.red()}  G: {color.green()}  B: {color.blue()} A: {color.alpha()}")
 		self.setToolTip(color.name())
+
+		print(f"{palettes.palette_is_dark_mode(self.palette())=}")
 	
 	@QtCore.Slot(QtGui.QPalette.ColorRole)
 	def setColorRole(self, role:QtGui.QPalette.ColorRole):
@@ -72,9 +78,27 @@ class ColorPaletteLabel(QtWidgets.QLabel):
 		self._color_group = group
 		self._updateColor()
 
-	def setPalette(self, arg__1):
-		super().setPalette(arg__1)
-		self._updateColor()
+class PaletteMaker(QtWidgets.QWidget):
+
+	def __init__(self, *args, **kwargs):
+
+		super().__init__(*args, **kwargs)
+
+		self.setLayout(QtWidgets.QFormLayout())
+
+		self._txt_fg = QtWidgets.QSpinBox(value=self.palette().windowText().color().lightness(), minimum=0, maximum=255)
+		self._txt_bg = QtWidgets.QSpinBox(value=self.palette().window().color().lightness(), minimum=0, maximum=255)
+
+		self.layout().addRow(
+			QtWidgets.QLabel("FG:"),
+			self._txt_fg
+		)
+
+		self.layout().addRow(
+			QtWidgets.QLabel("BG"),
+			self._txt_bg
+		)
+
 
 class MyKewlPaletteViewer(QtWidgets.QWidget):
 
@@ -91,13 +115,27 @@ class MyKewlPaletteViewer(QtWidgets.QWidget):
 
 		self.layout().addWidget(self._cmb_colorgroups, 0, 0, 1, 2)
 
+		# Store swatches so we can update them all when color group changes
+		self._swatches = []
+
 		for row, color_role in enumerate(ORDERED_COLOR_ROLES):
 
 			swatch = ColorPaletteLabel(color_role)
-			self._cmb_colorgroups.activated.connect(lambda: swatch.setColorGroup(self._cmb_colorgroups.currentData()))
+			self._swatches.append(swatch)
 
 			self.layout().addWidget(QtWidgets.QLabel(color_role.name), row+1, 0)
 			self.layout().addWidget(swatch, row+1, 1)
+		
+		# Connect after all swatches are created
+		self._cmb_colorgroups.currentIndexChanged.connect(self.setSwatchColorGroups)
+	
+	@QtCore.Slot()
+	def setSwatchColorGroups(self):
+
+		color_group = self._cmb_colorgroups.currentData()
+
+		for swatch in self._swatches:
+			swatch.setColorGroup(color_group)
 	
 if __name__ == "__main__":
 
@@ -106,5 +144,29 @@ if __name__ == "__main__":
 
 	wnd = MyKewlPaletteViewer()
 	wnd.show()
+
+	ctrl = PaletteMaker()
+	ctrl.show()
+
+	ctrl._txt_bg.valueChanged.connect(lambda:
+		wnd.setPalette(
+			palettes.build_palette(
+				fg_color=QtGui.QColor(*[ctrl._txt_fg.value()]*3),
+				bg_color=QtGui.QColor(*[ctrl._txt_bg.value()]*3),
+			)
+		))
+	
+	ctrl._txt_fg.valueChanged.connect(lambda:
+		wnd.setPalette(
+			palettes.build_palette(
+				fg_color=QtGui.QColor(*[ctrl._txt_fg.value()]*3),
+				bg_color=QtGui.QColor(*[ctrl._txt_bg.value()]*3),
+			)
+		))
+	
+	bw = binwidget.BSBinContentsWidget()
+	bw.show()
+
+	
 
 	sys.exit(app.exec())
