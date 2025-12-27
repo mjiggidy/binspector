c7dcddf434e63c9d972cc1e2e6d119bfa0df2e1c
Michael Jordan <michael@glowingpixel.com>
2025-12-26 16:56:36 -0800

    Clip color drawing

--- src/binspector/frameview/sceneitems.py
+++ src/binspector/frameview/sceneitems.py
@@ -1,6 +1,7 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 import avbutils
 from ..core.config import BSFrameViewConfig
+from ..utils import drawing
 from . import painters
 
 class BSFrameModeItem(QtWidgets.QGraphicsItem):
@@ -17,15 +18,67 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 		self._item_margins = BSFrameViewConfig.DEFAULT_ITEM_MARGINS
 		self._thumb_size   = QtCore.QSizeF(16,9)
 
-		self._label_margins = QtCore.QMarginsF(*[0.8]*4)
+		self._label_margins = QtCore.QMarginsF(*[0.4]*4)
 
 		self._brushes      = brush_manager
 		
+	
+	###
+	# Properties
+	###
+
+	@QtCore.Slot(object)
+	def setBrushManager(self, brush_manager:painters.BSFrameItemBrushManager):
+		self._brushes = brush_manager
+
 	def brushManager(self) -> painters.BSFrameItemBrushManager:
 		return self._brushes
 	
-	def setBrushManager(self, brush_manager:painters.BSFrameItemBrushManager):
-		self._brushes = brush_manager
+	@QtCore.Slot(str)
+	def setName(self, name:str):
+
+		if self._name == name:
+			return
+		
+		self._name = name
+		self.setToolTip(self._name)
+		self.update(self.labelRect())
+
+	def name(self) -> str:
+
+		return self._name
+
+	@QtCore.Slot(QtGui.QColor)
+	def setClipColor(self, clip_color:QtGui.QColor):
+
+		clip_color = QtGui.QColor(clip_color)
+
+		if self._clip_color == clip_color:
+			return
+		
+		self._clip_color = clip_color
+
+	def clipColor(self) -> QtGui.QColor:
+
+		return self._clip_color
+
+	@QtCore.Slot(object)
+	def setClipType(self, clip_type:avbutils.bins.BinDisplayItemTypes):
+
+		if self._clip_type == clip_type:
+			return
+		
+		self._clip_type = clip_type
+		self.update(self.labelRect())
+
+	def clipType(self) -> avbutils.bins.BinDisplayItemTypes:
+
+		return self._clip_type
+	
+
+	###
+	# Sizes
+	##
 
 	def setItemSize(self, item_size:QtCore.QSizeF):
 		"""Set the item size.  Recommend: one grid unit"""
@@ -54,6 +107,10 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 		"""Frame thumbnail aspect ratio"""
 		
 		return self._thumb_size
+	
+	###
+	# Rects
+	###
 	
 	def thumbRect(self) -> QtCore.QRectF:
 
@@ -89,6 +146,10 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 		"""The active area with margins and padding calculated.  Should be one grid unit minus padidng."""
 
 		return self.boundingRect().marginsRemoved(self._item_margins)
+	
+	###
+	# Painting
+	###
 
 	def paint(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionGraphicsItem, *args,  widget:QtWidgets.QWidget = ...):
 
@@ -96,6 +157,7 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 
 		self._draw_base(painter, option, widget)
 		self._draw_thumb(painter, option, widget)
+		self._draw_color_chip(painter, option, widget)
 		
 		if self._clip_color.isValid():
 			self._draw_clip_color(painter, option, widget)
@@ -104,7 +166,12 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 			self._draw_selection(painter, option, widget)
 
 		self._draw_label(painter, option, widget)
+
 		painter.restore()
+
+	###
+	# Drawerings codes 4 u
+	###
 
 	def _draw_base(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionGraphicsItem, widget:QtWidgets.QWidget):
 		
@@ -127,13 +194,17 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 		
 		
 		text_line_height = QtCore.QPointF(0, self._brushes.font_metrics.lineSpacing())
-		line_2_rect = self.labelRect().translated(text_line_height)
 
-		text_formatted = self._brushes.font_metrics.elidedText(self._name, QtGui.Qt.TextElideMode.ElideMiddle, self.labelRect().width())
+		line_1_rect = self.labelRect()
+		line_1_rect.adjust(line_1_rect.height()/2.5,0,0,0)
+
+		line_2_rect = line_1_rect.translated(text_line_height)
+
+		text_formatted = self._brushes.font_metrics.elidedText(self._name, QtGui.Qt.TextElideMode.ElideMiddle, line_1_rect.width())
 		
 		painter.setPen(self._brushes.pen_selected) if self.isSelected() else painter.setPen(self._brushes.pen_label)	
 		painter.setFont(font_name)
-		painter.drawText(self.labelRect(),  text_formatted, o=self._brushes.options_label)
+		painter.drawText(line_1_rect,  text_formatted, o=self._brushes.options_label)
 		
 		painter.setFont(font_type)
 		painter.drawText(line_2_rect, str(self._clip_type), o=self._brushes.options_label)
@@ -155,35 +226,31 @@ class BSFrameModeItem(QtWidgets.QGraphicsItem):
 		
 		painter.drawRect(rect_clip_color)
 
-	@QtCore.Slot(str)
-	def setName(self, name:str):
+	def _draw_color_chip(self, painter:QtGui.QPainter, option:QtWidgets.QStyleOptionGraphicsItem, widget:QtWidgets.QWidget):
 
-		if self._name == name:
-			return
+		painter.save()
+		scale = painter.transform().m11()
+		painter.scale(1/scale, 1/scale)
+
+		rect_chip = QtCore.QRectF(
+			QtCore.QPointF(self.labelRect().left() * scale, (self.labelRect().top()+0.2) * scale),
+			QtCore.QSizeF(
+				self.labelRect().height() * scale / 2 * 0.5,
+				self.labelRect().height() * scale / 2 * 0.5,
+			)
+		)
+		try:
+			drawing.draw_clip_color_chip(
+				painter=painter,
+				canvas=rect_chip,
+				clip_color=self._clip_color,
+				border_width=1/scale * 2,
+				border_color=self._brushes.pen_label.color(),
+				shadow_offset=QtCore.QPointF(scale/8,scale/8),
+				margins=QtCore.QMarginsF(0,0,0,0)
+			)
+		except Exception as e:
+			print(e)
+
+		painter.restore()
 		
-		self._name = name
-		self.setToolTip(self._name)
-		self.update(self.labelRect())
-
-	@QtCore.Slot(QtGui.QColor)
-	def setClipColor(self, clip_color:QtGui.QColor):
-
-		clip_color = QtGui.QColor(clip_color)
-
-		if self._clip_color == clip_color:
-			return
-		
-		self._clip_color = clip_color
-
-	def clipType(self) -> avbutils.bins.BinDisplayItemTypes:
-
-		return self._clip_type
-	
-	@QtCore.Slot(object)
-	def setClipType(self, clip_type:avbutils.bins.BinDisplayItemTypes):
-
-		if self._clip_type == clip_type:
-			return
-		
-		self._clip_type = clip_type
-		self.update(self.labelRect())
