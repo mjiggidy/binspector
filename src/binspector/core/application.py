373dbe6d5a6a3abebdd562e842537698da6c0c86
Michael Jordan <michael@glowingpixel.com>
2026-01-19 17:05:40 -0800

    Bump

--- src/binspector/core/application.py
+++ src/binspector/core/application.py
@@ -20,7 +20,7 @@ class BSMainApplication(QtWidgets.QApplication):
 		super().__init__(*args, **kwargs)
 
 		self.setApplicationName("Binspector")
-		self.setApplicationVersion("0.0.13")
+		self.setApplicationVersion("0.0.14")
 		self.setStyle("Fusion")
 
 		self.setOrganizationName("GlowingPixel")
