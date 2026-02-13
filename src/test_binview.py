4eb93a061f00bc11e993aafd8c2501824b75a651
Michael Jordan <michael@glowingpixel.com>
2026-02-13 12:40:29 -0800

    Start column header reorder thing

--- src/test_binview.py
+++ src/test_binview.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 
 from binspector.binview import binviewmodel, binviewitems
 from binspector.vieweditor import editorwidget
@@ -7,6 +7,24 @@ from binspector.textview import textview
 from binspector.models import viewmodels, viewmodelitems
 import avb
 
+@QtCore.Slot(int, int, int)
+def headerMoved(idx_logical, idx_old_vis, idx_new_vis):
+	
+	moved_header_name = wnd_bin.model().headerData(idx_logical, QtCore.Qt.Orientation.Horizontal, binviewitems.BSBinColumnInfoRole.DisplayNameRole)
+
+	if idx_new_vis > 0:
+		leading_header_logical_index = wnd_bin.header().logicalIndex(idx_new_vis-1)
+		leading_header_name = wnd_bin.model().headerData(leading_header_logical_index, QtCore.Qt.Orientation.Horizontal, binviewitems.BSBinColumnInfoRole.DisplayNameRole)
+		print(f"{moved_header_name} moved after {leading_header_name}")
+
+		# Need to map back to proxy stuff
+		new_logical_index = leading_header_logical_index + 1
+	
+	else:
+		print(f"{moved_header_name} moved to front")
+		new_logical_index = 0
+
+	print("New logical index: ", new_logical_index)
 
 if __name__ == "__main__":
 
@@ -17,6 +35,11 @@ if __name__ == "__main__":
 
 	
 	wnd_bin = textview.BSBinTextView()
+	
+	
+	# When section moved via header, find the logical index of the preceding visible visual index
+	# And move the BinCOlumnInfo to the next index in the model I think
+	wnd_bin.header().sectionMoved.connect(headerMoved)
 
 	bin_item_model = viewmodels.BSBinItemViewModel()
 	
@@ -24,7 +47,7 @@ if __name__ == "__main__":
 
 
 	with avb.open(sys.argv[1]) as bin_handle:
-		bin_view_info = binviewitems.BSBinViewInfo.from_binview(bin_handle.content.view_setting)
+		bin_view_info = binviewitems.BSBinView.from_binview(bin_handle.content.view_setting)
 	
 	bin_view_model = binviewmodel.BSBinViewModel(bin_view_info)
 	bin_item_model.setBinViewModel(bin_view_model)
@@ -37,3 +60,4 @@ if __name__ == "__main__":
 	wnd_bin.show()
 
 	app.exec()
+
