53d422fbcac8a2937e2caffc4d635a6ee8e1a236
Michael Jordan <michael@glowingpixel.com>
2026-02-19 12:06:23 -0800

    Begin data

--- src/binspector/binitems/binitemsmodel.py
+++ src/binspector/binitems/binitemsmodel.py
@@ -1,4 +1,12 @@
+
+import typing
 from PySide6 import QtCore
+from avbutils import bins
+
+from . import binitemtypes
+
+type BSBinItemModelEntry = dict[bins.BinColumnFieldIDs, binitemtypes.BSAbstractViewItem]
+"""Each bin item in the model is a dict of BinColumnFieldID and respective view item pairs"""
 
 class BSBinItemModel(QtCore.QAbstractItemModel):
 	"""Single-column item model for bin items"""
@@ -7,7 +15,7 @@ class BSBinItemModel(QtCore.QAbstractItemModel):
 
 		super().__init__(*args, **kwargs)
 
-		self._bin_items = []
+		self._bin_items:list[BSBinItemModelEntry] = []
 
 	def rowCount(self, /, parent:QtCore.QModelIndex) -> int:
 		"""Number of bin items"""
@@ -28,3 +36,40 @@ class BSBinItemModel(QtCore.QAbstractItemModel):
 		"""Model is flat, children have no parents"""
 		
 		return QtCore.QModelIndex()
+	
+	def index(self, row:int, column:int, /, parent:QtCore.QModelIndex) -> QtCore.QModelIndex:
+		
+		if parent.isValid():
+			return QtCore.QModelIndex()
+		
+		if column > self.columnCount(parent):
+			return QtCore.QModelIndex()
+		
+		if row > self.rowCount(self.parent):
+			return QtCore.QModelIndex()
+		
+		return self.createIndex(row, column)
+	
+	def data(self, index:QtCore.QModelIndex, /, role:binitemtypes.BSBinItemDataRoles) -> typing.Any:
+		
+		if not index.isValid():
+			return None
+		
+		bin_item = self._bin_items[index.row()]
+		
+		# Map BinItemDataRoles to their avbutils counterparts
+		if role == binitemtypes.BSBinItemDataRoles.ItemNameRole:
+			return bin_item.get(bins.BinColumnFieldIDs.Name).raw_data()
+		
+		elif role == binitemtypes.BSBinItemDataRoles.FrameCoordinatesRole:
+			# NOTE: TODO
+			return [100,100]
+		
+		elif role == binitemtypes.BSBinItemDataRoles.ClipColorRole:
+			return bin_item.get(bins.BinColumnFieldIDs.Color).raw_data()
+		
+		elif role == binitemtypes.BSBinItemDataRoles.ItemTypesRole:
+			return bin_item.get(bins.BinColumnFieldIDs.BinItemIcon).raw_data()
+		
+		elif role == binitemtypes.BSBinItemDataRoles.ScriptNotesRole:
+			return self._getUserColumnItem(index, user_column_name="Comments", role=QtCore.Qt.ItemDataRole.DisplayRole)
