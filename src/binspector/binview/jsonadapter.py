80b18ed6add113db770ac6f3ceb24f10061828fe
Michael Jordan <michael@glowingpixel.com>
2026-03-09 14:28:56 -0700

    BinView JSON Exporter

---
+++ src/binspector/binview/jsonadapter.py
@@ -0,0 +1,43 @@
+
+from __future__ import annotations
+import json
+from . import binviewitemtypes
+
+CURRENT_VERSION = "1.0"
+
+class BSBinViewJsonAdapter:
+	"""JSON Importer/Exporter for a binview"""
+
+	@staticmethod
+	def to_json(binview_info:binviewitemtypes.BSBinViewInfo) -> str:
+
+		return json.dumps({
+			"name": binview_info.name,
+			"version": CURRENT_VERSION,
+			"columns": [h.to_json_dict() for h in binview_info.columns]
+		}, indent="\t")
+	
+	@staticmethod
+	def from_json(json_string:str) -> binviewitemtypes.BSBinViewInfo:
+
+		binview_parsed = json.loads(json_string)
+
+		if binview_parsed.get("version") != CURRENT_VERSION:
+			raise ValueError("Unsupported binview json version")
+		
+		return binviewitemtypes.BSBinViewInfo(
+			name    = binview_parsed.get("name", "NoName"),
+			columns = [BSBinViewJsonAdapter._column_from_json(col) for col in binview_parsed.get("columns",[])]
+		)
+		
+	@staticmethod
+	def _column_from_json(column_json:str) -> binviewitemtypes.BSBinViewColumnInfo:
+
+		import avbutils
+
+		return binviewitemtypes.BSBinViewColumnInfo(
+			field_id     = avbutils.bins.BinColumnFieldIDs[column_json["field_id"]],
+			format_id    = avbutils.bins.BinColumnFormat[column_json["format_id"]],
+			display_name = column_json["display_name"],
+			is_hidden    = bool(column_json["is_hidden"])
+		)
---
+++ src/test_binviewjson.py
@@ -0,0 +1,40 @@
+import sys, json
+import avb
+from binspector.binview import jsonadapter, binviewitemtypes, binviewmodel
+
+# GO ON, HACK ME
+FIRST_OUTPUT  = "/Users/mjordan/Desktop/01_bin_to_json.json"
+SECOND_OUTPUT = "/Users/mjordan/Desktop/02_json_to_json.json"
+THIRD_OUTPUT  = "/Users/mjordan/Desktop/03_json_binmodel_roundtrip.json"
+
+if __name__ == "__main__":
+
+	if len(sys.argv) < 2:
+	
+		import pathlib
+		sys.exit(f"Usage: {pathlib.Path(__file__).name} path/to/bin.avb")
+	
+	with avb.open(sys.argv[1]) as bin_handle:
+
+		bin_view = binviewitemtypes.BSBinViewInfo.from_binview(bin_handle.content.view_setting)
+
+	with open(FIRST_OUTPUT, "w") as json_handle:
+
+		print(jsonadapter.BSBinViewJsonAdapter.to_json(bin_view), file=json_handle)
+
+	with open(FIRST_OUTPUT) as json_handle:
+
+		bin_view = jsonadapter.BSBinViewJsonAdapter.from_json(json_handle.read())
+
+	with open(SECOND_OUTPUT,"w") as json_handle:
+
+		print(jsonadapter.BSBinViewJsonAdapter.to_json(bin_view), file=json_handle)
+
+	with open(SECOND_OUTPUT) as json_handle:
+
+		bin_view_model = binviewmodel.BSBinViewModel()
+		bin_view_model.setBinViewInfo(jsonadapter.BSBinViewJsonAdapter.from_json(json_handle.read()))
+	
+	with open(THIRD_OUTPUT, "w") as json_handle:
+
+		print(jsonadapter.BSBinViewJsonAdapter.to_json(bin_view_model.binViewInfo()), file=json_handle)
