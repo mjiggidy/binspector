b307538e3d9a5f282340515e4779f290946d6888
Michael Jordan <michael@glowingpixel.com>
2026-04-02 09:26:57 -0700

    Explicit source model

--- src/binspector/views/filterstatsview.py
+++ src/binspector/views/filterstatsview.py
@@ -21,11 +21,12 @@ class BSFilterModelStatsView(QtWidgets.QLabel):
 
 	DEFAULT_TEXT = "Showing {filtered_item_count} of {source_item_count} items"
 
-	def __init__(self, *args, filter_model:QtCore.QSortFilterProxyModel|None=None, **kwargs):
+	def __init__(self, *args, filter_model:QtCore.QSortFilterProxyModel|None=None, source_model:QtCore.QAbstractItemModel|None=None, **kwargs):
 
 		super().__init__(*args, **kwargs)
 
-		self._model:QtCore.QSortFilterProxyModel = filter_model or QtCore.QSortFilterProxyModel(parent=self)
+		self._filter_model:QtCore.QSortFilterProxyModel = filter_model or QtCore.QSortFilterProxyModel(parent=self)
+		self._source_model:QtCore.QAbstractItemModel    = source_model or self._filter_model.sourceModel()
 
 		self._source_model_connections = []
 
@@ -33,12 +34,11 @@ class BSFilterModelStatsView(QtWidgets.QLabel):
 
 	def _setupModel(self):
 		
-		self._model.sourceModelChanged.connect(self._setupSourceModel)
+		self._filter_model.sourceModelChanged .connect(self._setupSourceModel)
+		self._filter_model.layoutChanged      .connect(self.modelDataChanged)
+		self._filter_model.modelReset         .connect(self.modelDataChanged)
 
-		self._model.layoutChanged.connect(self.modelDataChanged)
-		self._model.modelReset.connect(self.modelDataChanged)
-
-		if self._model.sourceModel() is not None:
+		if self._filter_model.sourceModel() is not None:
 			self._setupSourceModel()
 
 	@QtCore.Slot()
@@ -48,26 +48,26 @@ class BSFilterModelStatsView(QtWidgets.QLabel):
 			self.disconnect(c)
 
 		self._source_model_connections = [
-			self._model.sourceModel().rowsInserted    .connect(self.modelDataChanged),
-			self._model.sourceModel().columnsInserted .connect(self.modelDataChanged),
-			self._model.sourceModel().rowsRemoved     .connect(self.modelDataChanged),
-			self._model.sourceModel().columnsRemoved  .connect(self.modelDataChanged),
-			self._model.sourceModel().modelReset      .connect(self.modelDataChanged),
+			self._filter_model.sourceModel().rowsInserted    .connect(self.modelDataChanged),
+			self._filter_model.sourceModel().columnsInserted .connect(self.modelDataChanged),
+			self._filter_model.sourceModel().rowsRemoved     .connect(self.modelDataChanged),
+			self._filter_model.sourceModel().columnsRemoved  .connect(self.modelDataChanged),
+			self._filter_model.sourceModel().modelReset      .connect(self.modelDataChanged),
 		]
 
 	@QtCore.Slot()
 	def modelDataChanged(self):
 
-		if not self._model.sourceModel():
+		if not self._filter_model.sourceModel():
 			self.setText(self.tr("Nothing loaded"))
 			return
 		
-		if self._model.sourceModel().rowCount(QtCore.QModelIndex()) == 0:
+		if self._filter_model.sourceModel().rowCount(QtCore.QModelIndex()) == 0:
 			self.setText(self.tr("No items to show"))
 			return
 		
-		filtered_rows = self._model.rowCount(QtCore.QModelIndex())
-		source_rows   = self._model.rowCount(QtCore.QModelIndex())
+		filtered_rows = self._filter_model.rowCount(QtCore.QModelIndex())
+		source_rows   = self._filter_model.rowCount(QtCore.QModelIndex())
 
 		self.setText(self.tr(self.DEFAULT_TEXT).format(
 			filtered_item_count = QtCore.QLocale.system().toString(filtered_rows),
