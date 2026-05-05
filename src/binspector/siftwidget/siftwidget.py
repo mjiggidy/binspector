1ff391412f9f3e52ab92b2e30ccf5be46c450742
Michael Jordan <michael@glowingpixel.com>
2026-05-04 18:24:55 -0700

    Significant cleanup

--- src/binspector/siftwidget/siftwidget.py
+++ src/binspector/siftwidget/siftwidget.py
@@ -2,12 +2,12 @@
 Widgets for sifting bin contents
 """
 
-import typing
+import typing, logging
 
 from PySide6 import QtCore, QtWidgets
 from avbutils import bins
 
-from . import sourcesmodel
+from . import scopesmodel
 
 from .siftcriteriawidget import BSSiftCriteriaWidget
 from ..binfilters.siftfilter import sifters
@@ -16,14 +16,18 @@ class BSSiftSettingsWidget(QtWidgets.QWidget):
 	
 	CRITERIA_PER_SIFT = 3
 	DEFAULT_CRITERIA_TIMEOUT_MS = 200
+	DEFAULT_SIFT_CRITERIA = BSSiftCriteriaWidget.DEFAULT_SIFT_CRITERIA
 
-	sig_criteria_set = QtCore.Signal(object)
+	sig_criteria_set      = QtCore.Signal(object)
+	sig_live_sift_enabled = QtCore.Signal(bool)
 
-	def __init__(self, *args, bin_view_model:QtCore.QAbstractItemModel|None=None, **kwargs):
+	def __init__(self, *args, bin_view_model:QtCore.QAbstractItemModel|None=None, live_sift:bool=True, **kwargs):
 
 		super().__init__(*args, **kwargs)
 
-		self._columns_chooser_model = sourcesmodel.BSSiftSourcesViewModel(bin_view_model=bin_view_model or QtCore.QIdentityProxyModel())
+		self._columns_chooser_model = scopesmodel.BSSiftScopeViewModel(bin_view_model=bin_view_model or QtCore.QIdentityProxyModel())
+
+		self._live_sift_enabled = live_sift
 
 		self.setLayout(QtWidgets.QVBoxLayout())
 
@@ -34,31 +38,34 @@ class BSSiftSettingsWidget(QtWidgets.QWidget):
 
 		self.grp_sift_top    = QtWidgets.QGroupBox()
 		self.grp_sift_bottom = QtWidgets.QGroupBox()
-		#self.btn_dialog      = QtWidgets.QDialogButtonBox()
+	
+		self._sift_top_widgets:list[BSSiftCriteriaWidget] = []
+		self._sift_bot_widgets:list[BSSiftCriteriaWidget] = []
 		
+		self._criteria_changed_timer = QtCore.QTimer(parent=self, singleShot=True, interval=self.DEFAULT_CRITERIA_TIMEOUT_MS)
+		
+		self._chk_live_sift = QtWidgets.QCheckBox()
+
+		self._btn_apply = QtWidgets.QPushButton()
+		self._btn_clear = QtWidgets.QPushButton()
+
+		for _ in range(self.CRITERIA_PER_SIFT):
+			self._sift_top_widgets.append(BSSiftCriteriaWidget(sources_model=self._columns_chooser_model))
+
+		for _ in range(self.CRITERIA_PER_SIFT):
+			self._sift_bot_widgets.append(BSSiftCriteriaWidget(sources_model=self._columns_chooser_model))
+		
+		self._setupWidgets()
+		self._setupSignals()
+	
+	def _setupWidgets(self):
+
 		self.grp_sift_top.setLayout(QtWidgets.QVBoxLayout())
 		self.grp_sift_top.setTitle(self.tr("Show clips that meet this criteria:"))
 
 		self.grp_sift_bottom.setLayout(QtWidgets.QVBoxLayout())
 		self.grp_sift_bottom.setTitle(self.tr("Or, show clips that meet this criteria:"))
 
-#		self._chk_enable_sift = QtWidgets.QCheckBox()
-
-		self._sift_top_widgets:list[BSSiftCriteriaWidget] = []
-		self._sift_bot_widgets:list[BSSiftCriteriaWidget] = []
-
-		self._criteria_changed_timer = QtCore.QTimer(parent=self, singleShot=True, interval=self.DEFAULT_CRITERIA_TIMEOUT_MS)
-		self._criteria_changed_timer.timeout.connect(self.siftCriteriaSettled)
-
-#		self.btn_dialog.setStandardButtons(
-#			QtWidgets.QDialogButtonBox.StandardButton.Apply|QtWidgets.QDialogButtonBox.StandardButton.Cancel|QtWidgets.QDialogButtonBox.StandardButton.Ok|QtWidgets.QDialogButtonBox.StandardButton.Reset
-#		)
-#		self.btn_dialog.button(QtWidgets.QDialogButtonBox.StandardButton.Reset).setText(self.tr("Clear"))
-
-#		self._chk_enable_sift.setText("Enable Sift")
-#		self._chk_enable_sift.toggled.connect(lambda: self.sig_options_set.emit(*self.siftCriteria()))
-#		self.layout().addWidget(self._chk_enable_sift)
-		
 		self.layout().addWidget(self.grp_sift_top)
 
 		#self.grp_sift_top   .layout().setContentsMargins(2,2,2,2)
@@ -67,46 +74,83 @@ class BSSiftSettingsWidget(QtWidgets.QWidget):
 		#self.grp_sift_bottom.layout().setContentsMargins(2,2,2,2)
 		self.grp_sift_bottom.layout().setSpacing(2)
 
-
-		for _ in range(self.CRITERIA_PER_SIFT):
-
-			wdg = BSSiftCriteriaWidget()
-			self._sift_top_widgets.append(wdg)
-
-			wdg.setColumnsChooserModel(self._columns_chooser_model)
-			
-			wdg.sig_criteria_set.connect(self.siftCriteriaChanged)
-			
+		for wdg in self._sift_top_widgets:
 			self.grp_sift_top.layout().addWidget(wdg)
 
 		self.layout().addWidget(self.grp_sift_bottom)
 
-		for _ in range(self.CRITERIA_PER_SIFT):
-			
-			wdg = BSSiftCriteriaWidget()
-			self._sift_bot_widgets.append(wdg)
-			
-			wdg.setColumnsChooserModel(self._columns_chooser_model)
-			
-			wdg.sig_criteria_set.connect(self.siftCriteriaChanged)
-			
+		for wdg in self._sift_bot_widgets:
 			self.grp_sift_bottom.layout().addWidget(wdg)
 
-		#self.layout().addWidget(self.btn_dialog)
-	
+		self._chk_live_sift.setText(self.tr("Live Sift"))
+		self._chk_live_sift.setChecked(self._live_sift_enabled)
+		self._chk_live_sift.toggled.connect(self.setLiveSiftEnabled)
+				
+		self._btn_apply.setText(self.tr("Apply"))
+		self._btn_apply.setDefault(True)
+		self._btn_apply.setDisabled(self._live_sift_enabled)		
+
+		self._btn_clear.setText(self.tr("Clear"))
+
+		lay_buttons = QtWidgets.QHBoxLayout()
+		lay_buttons.addWidget(self._chk_live_sift)
+		lay_buttons.addStretch()
+		lay_buttons.addWidget(self._btn_clear)
+		lay_buttons.addWidget(self._btn_apply)
+
+		self.layout().addLayout(lay_buttons)
+
+	def _setupSignals(self):
+
+		self._criteria_changed_timer.timeout.connect(self.siftCriteriaSettled)
+		self._btn_apply.clicked.connect(self.siftCriteriaSettled)
+		self._btn_clear.clicked.connect(self.resetAllCriteria)
+
+		for wdg in self._sift_top_widgets:
+			wdg.sig_criteria_set.connect(self.siftCriteriaChanged)
+
+		for wdg in self._sift_bot_widgets:
+			wdg.sig_criteria_set.connect(self.siftCriteriaChanged)
+
+	@QtCore.Slot(bool)
+	def setLiveSiftEnabled(self, is_enabled:bool):
+		"""
+		If Live Sift is enabled, `sig_criteria_set` will emit on live changes.\n
+		Without Live Sift, `sig_criteria_set` will emit only when the user submits changes
+		"""
+
+		if self._live_sift_enabled == is_enabled:
+			return
+		
+		logging.getLogger(__name__).debug(f"Live Sift set to %s", is_enabled)
+		
+		self._live_sift_enabled = is_enabled
+
+		self._btn_apply.setDisabled(self._live_sift_enabled)
+
+		self.sig_live_sift_enabled.emit(is_enabled)
+
+	def liveSiftEnabled(self) -> bool:
+		"""Is widget reporting changes as they happen"""
+
+		return self._live_sift_enabled
+
 	@QtCore.Slot(object)
 	def setBinViewModel(self, bin_view_model:QtCore.QAbstractItemModel):
+		"""Set the bin view model for the sources chooser"""
 
 		self._columns_chooser_model.setBinViewModel(bin_view_model)
-
 	
 	@QtCore.Slot()
 	def siftCriteriaChanged(self):
+		"""Sift criteria was changed"""
 
-		self._criteria_changed_timer.start()
+		if self._live_sift_enabled:
+			self._criteria_changed_timer.start()
 
 	@QtCore.Slot()
 	def siftCriteriaSettled(self):
+		"""Sift criteria is final"""
 
 		self.sig_criteria_set.emit(self.siftCriteria())
 
@@ -114,6 +158,15 @@ class BSSiftSettingsWidget(QtWidgets.QWidget):
 		"""The sift criteria as currently set"""
 
 		return (
-			[s.siftCriteria() for s in self._sift_top_widgets],
-			[s.siftCriteria() for s in self._sift_bot_widgets]
+			[s.criteria() for s in self._sift_top_widgets],
+			[s.criteria() for s in self._sift_bot_widgets]
 		)
+	
+	@QtCore.Slot()
+	def resetAllCriteria(self):
+
+		for w in self._sift_top_widgets:
+			w.setCriteria(self.DEFAULT_SIFT_CRITERIA)
+
+		for w in self._sift_bot_widgets:
+			w.setCriteria(self.DEFAULT_SIFT_CRITERIA)
